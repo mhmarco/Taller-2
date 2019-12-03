3364580ddbb91b517211e87062bbd12b1741a243
ulacit <ulacit@docente.ulacit.ac.cr>
2019-12-02 19:34:07 -0600

    Add components

---
+++ Taller2/Taller2/Components/AComponent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// </summary>
+///
+namespace Taller2.Components
+{
+    public abstract class AComponent
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public double ExpectedRequiredtime { get; set; }
+        private double Price = 0;
+
+        private List<AComponent> components = new List<AComponent>();
+
+        public AComponent(double price)
+        {
+            Price = price;
+        }
+
+        public void Add(AComponent component)
+        {
+            components.Add(component);
+        }
+        public void Eliminar(AComponent component)
+        {
+            components.Remove(component);
+        }
+        public virtual double GetPrice()
+        {
+            foreach (var component in components);
+
+            return Price * 0.75;
+        }
+    }
+}
---
+++ Taller2/Taller2/Components/PhoneScreen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2.Components
+{
+    public class PhoneScreen : AComponent
+    {
+        public PhoneScreen(double price) : base(price)
+        {
+            Number = 101;
+            Name = "Phone Screen";
+            ExpectedRequiredtime = 3;
+
+        }
+    }
+}
