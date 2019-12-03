5bf5e0143c899f8160f49ca871185a15349b31f4
Marco Antonio Morales Hidalgo <mmoralesh491@ulacit.ed.cr>
2019-12-02 19:41:24 -0600

    ajan

---
+++ Taller2/Taller2/Components/Battery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2.Components
+{
+    class Battery : AComponent
+    {
+        public Battery(double price) : base(price)
+        {
+            Number = 104;
+            Name = "Battery";
+            ExpectedRequiredtime = 2;
+
+        }
+    }
+}
